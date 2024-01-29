d4033f5e930ac9af9c19bd6312ade432200e504d
Miad Hasan <mdhasan9.sy@gmail.com>
2024-01-30 00:25:45 +0600

    init commit

---
+++ bank.py
@@ -0,0 +1,39 @@
+while True:
+    print("1. Create a new account")
+    print("2. Display all accounts")
+    print("3. Update an account")
+    print("4. Delete an account")
+    print("5. Deposit an amount into your account")
+    print("6. Withdraw an amount from your account")
+    print("7. Search for account")
+    print("8. Exit")
+
+    choice = input("Enter your choice (1-8): ")
+
+    if choice == "1":
+        print(choice)
+
+    elif choice == "2":
+        print(choice)
+
+    elif choice == "3":
+        print(choice)
+
+    elif choice == "4":
+        print(choice)
+
+    elif choice == "5":
+        print(choice)
+
+    elif choice == "6":
+        print(choice)
+
+    elif choice == "7":
+        print(choice)
+
+    elif choice == "8":
+        print(choice)
+        break
+
+    else:
+        print("Invalid choice. Please enter a number between 1 and 8.")
